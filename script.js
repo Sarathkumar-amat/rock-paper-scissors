17be6fb22b0a05326937fe6345efdaa5759f1189
sarath23 <69066317+Sarathkumar-amat@users.noreply.github.com>
2023-09-11 16:00:01 +0530

    added game function

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ const array = ["rock","paper","scissor"];
 function getComputerChoice(){
     return array[(Math.floor(Math.random() * array.length))];
 }
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
 
 function playRound(playerSelection,computerSelection)
 {
@@ -35,3 +29,39 @@ function playRound(playerSelection,computerSelection)
         else if(computer==="PAPER") return "You Win";
     }
 } 
+
+function game()
+{
+    let myScore = 0;
+    let computerScore = 0;
+    for(let i=1;i<=5;i++)
+    {
+        let playerChoice = prompt("Enter your choice(Rock or Paper or Scissor");
+        let result = playRound(playerChoice,getComputerChoice());
+        if(result==="TIE")
+        {
+            myScore++;
+            computerScore++;
+        }
+        else if(result==="You Win")
+        {
+            myScore++;
+        }
+        else if(result==="You Lose")
+        {
+            computerScore++;
+        }
+        console.log(`Game ${i}: ${result}`);
+    }
+    if(myScore>computerScore)
+    {
+        console.log("You won this game")
+    }
+    else if(myScore<computerScore){
+        console.log("You lost this game")
+    }
+    else{
+        console.log("Game Tie");
+    }
+}
+game();
